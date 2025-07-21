f9415309d0916ccb24bc354f14bce5aa193071ad
Sahil Bambarkar <162725936+sahilbambarkar@users.noreply.github.com>
2025-07-21 18:38:47 +0530

    Delete src directory

--- src/service/utiles/createid.jsx
+++
@@ -1,7 +0,0 @@
-const Createid = () => {
-  const timestamp = new Date().toISOString().replace(/[-:.]/g, "");
-  const id = timestamp + Math.random().toString(12).substring(2, 8);
-  return id;
-};
-
-export default Createid;
