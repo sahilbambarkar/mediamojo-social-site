931a17bf761209ab5025a7150c2fe478f6acde54
Sahil Bambarkar <162725936+sahilbambarkar@users.noreply.github.com>
2025-07-24 14:40:57 +0530

    btn size changed

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -10,7 +10,7 @@ export default function ModeToggle() {
     return (
         <Button
             variant="outline"
-            size="icon"
+            size="sm"
             onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
         >
             <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
